433892174e44945080f97b0f6b92dab09505189f
nextgtrgod <galinaborissenko@gmail.com>
2019-06-01 18:33:10 +0300

    first commit

---
+++ src/data.js
@@ -0,0 +1,9 @@
+let photos = [
+	require('../gal/STAR_WARS_sci_fi_futuristic_artwork_disney_1600x1200.jpg'), 
+	require('../gal/minimalistic_artwork_vector_art_2560x1440.jpg'),
+	require('../gal/Skyline_new_york_city_1920x1200.jpg'), 
+	require('../gal/vector_digital_art_1600x1200.jpg'),
+	require('../gal/Threadless_cart_fun_art_1920x1200.jpg'),
+]
+
+export default photos
---
+++ src/init.js
@@ -0,0 +1,74 @@
+import photos from './data'
+
+let init = () => {
+
+	let list = document.querySelector('.list')
+	
+	let imgNodes = []
+	
+	for (let i = 0; i < photos.length; i++) { // Display a list of photos
+		let img = document.createElement('img')
+	
+		imgNodes.push(img)
+	
+		img.src = photos[i]
+		list.appendChild(img) // added to item list img
+	
+		img.addEventListener('click', function () {
+			n = i
+			set()
+		})
+	}
+	
+	console.log(imgNodes);
+	let full = document.querySelector('.full')
+	let imgBig = document.getElementById('full-img')
+	
+	let set = () => {
+		imgBig.src = photos[n]
+	
+		for (let i = 0; i < imgNodes.length; i++) {
+			imgNodes[i].classList.remove('active')
+		}
+	
+		imgNodes[n].classList.add('active')
+		
+	}
+	
+	
+	let right = document.querySelector('.right')
+	let n = 0
+	
+	set()
+	// full.appendChild(imgBig) //
+	
+	let next = () => {
+		n++ 
+		n = n % photos.length
+		set()
+	}
+	
+	right.addEventListener('click', next)
+	
+	let left = document.querySelector('.left')
+	
+	left.addEventListener('click', function () { // back preview
+		n-- 
+		n = n % photos.length
+		if (n < 0) {
+			n = photos.length - 1
+		}
+		set()
+	})
+	
+	//Focus stays on selected photo
+	
+	imgBig.addEventListener('click', next)
+	
+	document.addEventListener('keydown', function(e) {
+		e.keyCode === 39 && next()
+		// e.keyCode === 37 && prev()
+	})
+}
+
+export default init
