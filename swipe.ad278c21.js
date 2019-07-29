b77430df06c2fc1a5257ad1d81472d1e902ad416
nextgtrgod <galinaborissenko@gmail.com>
2019-07-30 00:44:36 +0300

    change the size 2

--- swipe.ad278c21.js
+++ swipe.ad278c21.js
@@ -164,7 +164,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "9674" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "10348" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
